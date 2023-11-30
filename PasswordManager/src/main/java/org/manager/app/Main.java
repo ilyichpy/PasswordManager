148cd4c340ba8fb57e2553873ef7527efc068aca
ilyichpy <ilyaszuev25@gmail.com>
2023-11-30 15:26:33 +0300

    done

--- PasswordManager/src/main/java/org/manager/app/Main.java
+++ PasswordManager/src/main/java/org/manager/app/Main.java
@@ -1,15 +1,34 @@
 package org.manager.app;
 
-import org.manager.repository.PasswordRepository;
-import org.manager.repository.PasswordRepositoryIml;
 import org.manager.service.PasswordServiceIml;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         PasswordServiceIml passwordService = new PasswordServiceIml();
-        passwordService.addKey();
-        passwordService.watchAllPasses();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = false;
+        while (true) {
+            System.out.println("чтобы добавить новый пароль введите 1\nЧтобы посмотреть всю информацию введите 2\nДля выхода введите exit");
+            String userInput = sc.nextLine();
+            switch (userInput) {
+                case "exit":
+                    flag = true;
+                    break;
+                case "1":
+                    passwordService.addKey();
+                    System.out.println();
+                    break;
+                case "2":
+                    passwordService.watchAllPasses();
+                    System.out.println();
+                    break;
+            }
+            if (flag) {
+                break;
+            }
+
+        }
     }
 }
